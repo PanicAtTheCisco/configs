a4602708d8e4129e4e616aec2d17ff24775ca6b1
PanicAtTheCisco <83227932+PanicAtTheCisco@users.noreply.github.com>
2023-12-07 20:51:08 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,57 +7,70 @@ then
   exit 1
 fi
 
-mkdir /home/$SUDO_USER/configs
-mkdir /home/$SUDO_USER/backup_configs
-
-wget -O /home/$SUDO_USER/configs/.zshrc https://raw.githubusercontent.com/PanicAtTheCisco/linux-configs/main/.zshrc
-wget -O /home/$SUDO_USER/configs/.vimrc https://raw.githubusercontent.com/PanicAtTheCisco/linux-configs/main/.vimrc
-wget -O /home/$SUDO_USER/configs/.p10k.zsh https://raw.githubusercontent.com/PanicAtTheCisco/linux-configs/main/.p10k.zsh
-
-installed=true
 packagesNeeded="wget git zsh"
+#Alpine
 if [ -x "$(command -v apk)" ];       
 then 
     sudo apk add --no-cache $packagesNeeded
+    finishConfigs
 
+#Debian/Ubuntu
 elif [ -x "$(command -v apt-get)" ]; 
 then 
     sudo apt install $packagesNeeded
+    finishConfigs
 
+#Fedora/Red Hat
 elif [ -x "$(command -v dnf)" ];     
 then 
     sudo dnf install $packagesNeeded
-s
+    finishConfigs
+
+#OpenSUSE
 elif [ -x "$(command -v zypper)" ];  
 then 
     sudo zypper install $packagesNeeded
+    finishConfigs
 
+#Arch
 elif [ -x "$(command -v pacman)" ];  
 then 
     sudo pacman -sS $packagesNeeded
+    finishConfigs
 
+#Alert if failed
 else 
     echo "FAILED TO INSTALL PACKAGE: Package manager not found. You must manually install: $packagesNeeded">&2; 
-    installed=false
 fi
 
-if [[ $installed == true ]]; then 
+echo "Hack Nerd Font will have to be manually installed from "https://github.com/ryanoasis/nerd-fonts/releases" and enabled."
+echo "May have to run 'p10k configure' to get icons to render correctly."
+
+echo "Install finished, restart your terminal to complete!"
+
+
+
+finishConfigs() {
+    mkdir /home/$SUDO_USER/configs
+    mkdir /home/$SUDO_USER/backup_configs
+
+    wget -O /home/$SUDO_USER/configs/.zshrc https://raw.githubusercontent.com/PanicAtTheCisco/linux-configs/main/.zshrc
+    wget -O /home/$SUDO_USER/configs/.vimrc https://raw.githubusercontent.com/PanicAtTheCisco/linux-configs/main/.vimrc
+    wget -O /home/$SUDO_USER/configs/.p10k.zsh https://raw.githubusercontent.com/PanicAtTheCisco/linux-configs/main/.p10k.zsh
+
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-    echo -n "Use zsh config? (y/n): "
-    read zsh_config
-    echo -n "Use p10k config? (y/n): "
-    read p10k_config
-    echo -n "Use vim config? (y/n): "
-    read vim_config
+    read -i "Use zsh config? (y/n): " zsh_config
+    read -i "Use p10k config? (y/n): " p10k_config
+    read -i "Use vim config? (y/n): " vim_config
 
     if [[ "$zsh_config" == [yY] ]]; then
         mv /home/$SUDO_USER/.zshrc /home/$SUDO_USER/backup_configs/.zshrc_old
         cp /home/$SUDO_USER/configs/.zshrc /home/$SUDO_USER/.zshrc
     fi
-    
+
     if [[ "$p10k_config" == [yY] ]]; then
         mv /home/$SUDO_USER/.p10k.zsh /home/$SUDO_USER/backup_configs/.p10k.zsh_old
         cp /home/$SUDO_USER/configs/.p10k.zsh /home/$SUDO_USER/.p10k.zsh
@@ -71,10 +84,5 @@ if [[ $installed == true ]]; then
     git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-    sudo chsh -s /usr/bin/zsh
-fi
-
-echo "Hack Nerd Font will have to be manually installed from "https://github.com/ryanoasis/nerd-fonts/releases" and enabled."
-echo "May have to run 'p10k configure' to get icons to render correctly."
-
-echo "Install finished, restart your terminal to complete!"
+    sudo chsh -s $(which zsh)
+}
