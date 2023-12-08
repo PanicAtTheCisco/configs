d67b75c2d8edfc5d098a4c79e3b1f453a8a04b64
PanicAtTheCisco <83227932+PanicAtTheCisco@users.noreply.github.com>
2023-12-07 21:18:39 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -21,6 +21,8 @@ finishConfigs() {
     mv ~/.vimrc ~/backup_configs/.vimrc_old
     cp ~/configs/.vimrc ~/.vimrc
 
+    sudo su
+
     git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 }
