8dc7ac4f45ad894b1cee4870f9202de905034050
PanicAtTheCisco <83227932+PanicAtTheCisco@users.noreply.github.com>
2023-12-07 21:19:06 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -21,10 +21,8 @@ finishConfigs() {
     mv ~/.vimrc ~/backup_configs/.vimrc_old
     cp ~/configs/.vimrc ~/.vimrc
 
-    sudo su
-
-    git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+    sudo git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+    sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 }
 
 # if [[ $EUID -ne 0 ]]
