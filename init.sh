d6cf9d54ff6c6b4e255239f6edef093bcb58b944
PanicAtTheCisco <83227932+PanicAtTheCisco@users.noreply.github.com>
2023-12-07 21:08:18 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 finishConfigs() {
-    mkdir /home/$SUDO_USER/configs
-    mkdir /home/$SUDO_USER/backup_configs
+    mkdir ~/configs
+    mkdir ~/backup_configs
 
-    wget -O /home/$SUDO_USER/configs/.zshrc https://raw.githubusercontent.com/PanicAtTheCisco/linux-configs/main/.zshrc
-    wget -O /home/$SUDO_USER/configs/.vimrc https://raw.githubusercontent.com/PanicAtTheCisco/linux-configs/main/.vimrc
-    wget -O /home/$SUDO_USER/configs/.p10k.zsh https://raw.githubusercontent.com/PanicAtTheCisco/linux-configs/main/.p10k.zsh
+    wget -O ~/configs/.zshrc https://raw.githubusercontent.com/PanicAtTheCisco/linux-configs/main/.zshrc
+    wget -O ~/configs/.vimrc https://raw.githubusercontent.com/PanicAtTheCisco/linux-configs/main/.vimrc
+    wget -O ~/configs/.p10k.zsh https://raw.githubusercontent.com/PanicAtTheCisco/linux-configs/main/.p10k.zsh
 
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
@@ -17,32 +17,32 @@ finishConfigs() {
     read -i "Use vim config? (y/n): " vim_config
 
     if [[ "$zsh_config" == [yY] ]]; then
-        mv /home/$SUDO_USER/.zshrc /home/$SUDO_USER/backup_configs/.zshrc_old
-        cp /home/$SUDO_USER/configs/.zshrc /home/$SUDO_USER/.zshrc
+        mv ~/.zshrc ~/backup_configs/.zshrc_old
+        cp ~/configs/.zshrc ~/.zshrc
     fi
 
     if [[ "$p10k_config" == [yY] ]]; then
-        mv /home/$SUDO_USER/.p10k.zsh /home/$SUDO_USER/backup_configs/.p10k.zsh_old
-        cp /home/$SUDO_USER/configs/.p10k.zsh /home/$SUDO_USER/.p10k.zsh
+        mv ~/.p10k.zsh ~/backup_configs/.p10k.zsh_old
+        cp ~/configs/.p10k.zsh ~/.p10k.zsh
     fi
 
     if [[ "$vim_config" == [yY] ]]; then
-        mv /home/$SUDO_USER/.vimrc /home/$SUDO_USER/backup_configs/.vimrc_old
-        cp /home/$SUDO_USER/configs/.vimrc /home/$SUDO_USER/.vimrc
+        mv ~/.vimrc ~/backup_configs/.vimrc_old
+        cp ~/configs/.vimrc ~/.vimrc
     fi
 
     git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-    sudo chsh -s $(which zsh)
+    chsh -s $(which zsh)
 }
 
-if [[ $EUID -ne 0 ]]
-then
-  printf 'Must be run as root, exiting!\n'
-  tput cnorm
-  exit 1
-fi
+# if [[ $EUID -ne 0 ]]
+# then
+#   printf 'Must be run as root, exiting!\n'
+#   tput cnorm
+#   exit 1
+# fi
 
 packagesNeeded="wget curl git zsh"
 #Alpine
@@ -76,6 +76,7 @@ else
     exit 1
 fi
 
+su $SUDO_USER
 finishConfigs
 
 echo "\nHack Nerd Font will have to be manually installed from "https://github.com/ryanoasis/nerd-fonts/releases" and enabled."
